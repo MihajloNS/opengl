bf244e94f89192a76a4da77c11732bc74a7a2daa
Mihajlo Marinkovic <mihajlomarinkovic@yahoo.com>
2021-01-12 13:50:50 +0100

    Add uniforms examaple

---
+++ examples/uniforms/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=${PWD}/../../dependencies/glfw/lib/:${PWD}/../../dependencies/glew/lib/
+
+# Override opengl implementation version
+export MESA_GL_VERSION_OVERRIDE=3.3
+
+export SHADER_PATH=${PWD}/res/Basic.shader
+
+./uniforms
